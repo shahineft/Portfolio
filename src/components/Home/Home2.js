ba21dc58b7818146c757692ef8ab1df1d580d677
Shahin <shahineft@gmail.com>
2023-07-29 15:34:51 +0200

    Minor Changes

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -33,7 +33,7 @@ function Home2() {
                             </i>
                             <br/>
                             <br/>
-                            My field of Interest's are building new &nbsp;
+                            My fields of Interest are building new &nbsp;
                             <i>
                                 <b className="purple">Back-end Services and Products </b> and
                                 also in areas related to{" "}
