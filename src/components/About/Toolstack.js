83758040aaabbc1ad0b97b66a2bd75203d80b41a
Shahin <shahineft@gmail.com>
2023-08-06 16:59:34 +0200

    Minor Changes

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -7,7 +7,10 @@ import {
     SiDiscord,
     SiVmware,
     SiAnaconda,
-    SiQt
+    SiQt,
+    SiVisualstudio,
+    SiWindows11,
+    SiSuse,
 } from "react-icons/si";
 
 function Toolstack() {
@@ -15,6 +18,29 @@ function Toolstack() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <SiUbuntu />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+          <SiWindows11/>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+          <SiAnaconda />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+          <SiQt/>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+          <SiSuse/>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+          <SiVisualstudio/>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+          <SiVmware />
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
@@ -28,20 +54,7 @@ function Toolstack() {
       <Col xs={4} md={2} className="tech-icons">
         <SiDiscord />
       </Col>
-
-      <Col xs={4} md={2} className="tech-icons">
-          <SiVmware />
-      </Col>
-
-        <Col xs={4} md={2} className="tech-icons">
-          <SiAnaconda />
-      </Col>
-
-      <Col xs={4} md={2} className="tech-icons">
-          <SiQt/>
-      </Col>
     </Row>
   );
 }
-
 export default Toolstack;
